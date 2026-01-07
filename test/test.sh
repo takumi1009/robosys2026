3a324ef35fc5225308506a5a445b0f4e2a1f7052
Takumi <s24c1052rg@s.chibakoudai.jp>
2026-01-07 18:07:03 +0900

    Add a test

---
+++ test/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2026 Komiya Takumi
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+set -e
+
+echo "千葉県" | ../pref2region > out1.txt
+grep -q "関東 千葉市" out1.txt
+
+echo "大阪府" | ../pref2region > out2.txt
+grep -q "近畿 大阪市" out2.txt
